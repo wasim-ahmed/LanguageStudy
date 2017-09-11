d7acac7fcf72f3daa2267b0acd9ac51fa1c456ed
wasim-ahmed <wasim_miet@yahoo.in>
2017-09-11 16:17:09 +0530

    Output:
    public_fun_A
    protected_fun_A

---
+++ Access Specifier/OOPS-Scope- Protected-Inheritance.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+using namespace std;
+
+class A
+{
+		private:
+		int private_var_A ;
+		void private_fun_A()
+		{
+			cout<<__FUNCTION__<<endl;
+		}
+	protected:
+		int protected_var_A ;
+		void protected_fun_A()
+		{
+			cout<<__FUNCTION__<<endl;
+		}		
+	public:
+		int public_var_A ;
+		void public_fun_A()
+		{
+			cout<<__FUNCTION__<<endl;
+		}
+};
+
+class B : protected A
+{
+		private:
+		int private_var_B ;
+		void private_fun_B()
+		{
+			public_fun_A();
+			protected_fun_A();
+			//private_fun_A();// private will not be accessible here
+		}
+	protected:
+		int protected_var_B ;
+		void protected_fun_B()
+		{
+			public_fun_A();
+			protected_fun_A();
+			//private_fun_A();// private will not be accessible here
+		}		
+	public:
+		int public_var_B ;
+		void public_fun_B()
+		{
+			public_fun_A();
+			protected_fun_A();
+			//private_fun_A();// private will not be accessible here
+		}
+};
+
+int main()
+{
+	B b_Obj;
+	b_Obj.public_fun_B();
+	//b_Obj.public_var_A = 10; //ERROR
+}
