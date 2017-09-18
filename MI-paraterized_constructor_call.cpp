0368b0d59ea28cb97902102ea9bbedde2b288ade
wasim-ahmed <wasim_miet@yahoo.in>
2017-09-18 22:23:46 +0530

    Add files via upload

---
+++ MI-paraterized_constructor_call.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+using namespace std;
+
+class base
+{
+	private:
+	
+		int x;
+	public:
+
+	base(int temp)
+	{
+		cout<<"base class parameterized constructor"<<endl;	
+		x = temp;
+	}
+	
+	
+	virtual void fun() =0;
+
+	protected:
+};
+
+class derived1 : virtual public base
+{
+	private:
+
+	public:
+
+		derived1():base(2) {}
+		void fun()
+		{
+			cout<<"derived1::"<<__FUNCTION__<<endl;
+		}	
+	
+	protected:
+};
+
+class derived2 : virtual public base
+{
+	private:
+
+	public:
+	
+		derived2():base(2) {}
+		void fun()
+		{
+			cout<<"derived2::"<<__FUNCTION__<<endl;
+		}
+
+	protected:
+};
+
+class regular : public derived1, public derived2
+{
+	private:
+
+	public:
+
+		regular():base(2) {}
+		void fun()
+		{
+			cout<<"regular ::"<<__FUNCTION__<<endl;
+		}
+	
+
+	protected:
+};
+
+int main()
+{
+
+	base *ptr1 = new regular();
+	ptr1->fun();
+
+	return 1;
+}
+
