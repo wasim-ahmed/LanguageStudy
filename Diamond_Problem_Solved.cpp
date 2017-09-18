023f56e2e97950b780486de14a45e7463a2242be
wasim-ahmed <wasim_miet@yahoo.in>
2017-09-18 15:52:50 +0530

    Add files via upload

---
+++ Diamond_Problem_Solved.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+using namespace std;
+
+class base
+{
+	private:
+
+	public:
+	
+		virtual void fun() = 0;
+
+	protected:
+};
+
+class derived1 : virtual public base
+{
+	private:
+
+	public:
+
+		void fun()
+		{
+			cout<<"derived1::"<<__FUNCTION__<<endl;
+		}	
+	
+	protected:
+};
+
+class derived2 : virtual public base
+{
+	private:
+
+	public:
+		void fun()
+		{
+			cout<<"derived2::"<<__FUNCTION__<<endl;
+		}
+
+	protected:
+};
+
+class regular : public derived1, public derived2
+{
+	private:
+
+	public:
+
+		void fun()
+		{
+			cout<<"regular ::"<<__FUNCTION__<<endl;
+		}
+	
+
+	protected:
+};
+
+int main()
+{
+	base *ptr = new derived1();
+	ptr->fun();
+
+	base *ptr1 = new regular();//can not upcast , which sub object?
+	ptr1->fun();
+	return 1;
+}
+
